21f28b2948e2538fbf9e24b3a2b866318573d395
Jonathan-Bullock <jonathanbullock94@yahoo.com>
2025-09-01 21:08:11 -0400

    Create Get-WANIP uses multiple services.ps1

---
+++ Get-WANIP uses multiple services.ps1
@@ -0,0 +1,4 @@
+﻿(Invoke-RestMethod -Uri "https://ipinfo.io/").ip
+(Invoke-WebRequest -Uri "https://api.ipify.org/" -UseBasicParsing).Content
+(Invoke-WebRequest -Uri "https://ifconfig.me/ip" -UseBasicParsing).Content
+nslookup myip.dnsfilter.com
