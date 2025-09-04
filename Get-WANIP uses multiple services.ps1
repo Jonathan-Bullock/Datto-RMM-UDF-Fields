a8c37fdaa3fcf7868310a3dc93f9a6f6112267ae
Jonathan-Bullock <jonathanbullock94@yahoo.com>
2025-09-04 10:49:17 -0400

    S1 Operational Status and WAN IP

--- Get-WANIP uses multiple services.ps1
+++ Get-WANIP uses multiple services.ps1
@@ -1,4 +1,21 @@
-﻿(Invoke-RestMethod -Uri "https://ipinfo.io/").ip
-(Invoke-WebRequest -Uri "https://api.ipify.org/" -UseBasicParsing).Content
-(Invoke-WebRequest -Uri "https://ifconfig.me/ip" -UseBasicParsing).Content
-nslookup myip.dnsfilter.com
+﻿$i = 0..3 #Initialize Array
+#check with multiple services for the clients network public IP and return the resolved address as a string
+$i[0] = (Invoke-RestMethod -Uri "https://ipinfo.io/").ip
+$i[1] = (Invoke-WebRequest -Uri "https://api.ipify.org/" -UseBasicParsing).Content
+$i[2] = (Invoke-WebRequest -Uri "https://ifconfig.me/ip" -UseBasicParsing).Content
+#This value is important since it is used to check if DNS filter is authenticated
+$i[3] = (nslookup myip.dnsfilter.com 2>$null | Select-String -Pattern "\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}" | ForEach-Object { $_.Matches.Value } | Select-Object -Index 1)
+#Group the values of the array and return the most common item
+$PublicIP = $i | Group-Object | Sort-Object Count -Descending | Select-Object -First 1 -ExpandProperty Name
+return $PublicIP
+
+
+<# Straght return#>
+cls
+write-host "$((Invoke-RestMethod -Uri "https://ipinfo.io/").ip) - IPInfo.io"
+write-host "$((Invoke-WebRequest -Uri "https://api.ipify.org/" -UseBasicParsing).Content) - ipify.org"
+write-host "$((Invoke-WebRequest -Uri "https://ifconfig.me/ip" -UseBasicParsing).Content) - ifconfig.me"
+write-host "$(Resolve-DnsName -Name myip.dnsfilter.com -Server 103.247.36.36) - dnsfilter.com"
+#>
+
+Resolve-DnsName -Name myip.dnsfilter.com -Server 103.247.36.36
---
+++ S1 Status to UDF 5.ps1
@@ -0,0 +1,23 @@
+﻿#find current version Config File
+if(!(test-path "C:\Program Files\SentinelOne\")){
+    Write-error "C:\Program Files\SentinelOne\ doesn't existing. Exiting early..."
+    exit 1}
+
+
+Set-Location  (Get-ChildItem -Path "C:\Program Files\SentinelOne\" -Recurse -Filter *sentinelctl*).DirectoryName
+
+#Run Queery's
+$s1_mgmtServer = .\sentinelctl configure server.mgmtServer
+$S1_Site = .\sentinelctl configure server.site
+$S1_vssSnapshots  = .\sentinelctl configure agent.vssSnapshots 
+
+#Build String
+$udf = "VSS Snapshots: " + $S1_vssSnapshots + ", Site ID: " + $S1_Site + ", MGMT SVR: " +$s1_mgmtServer
+#write sting to terminal
+$udf
+
+#Write to registry for Datto RMM UDF
+Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom5$env:usrUDF5" -Value $udf5
+
+#used for Performance Tracking
+Get-Date
