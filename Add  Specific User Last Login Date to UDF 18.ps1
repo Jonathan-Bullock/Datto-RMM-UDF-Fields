536328fb23c05ba714a4b239de23cfb193456a1c
Jonathan-Bullock <88416014+Jonathan-Bullock@users.noreply.github.com>
2025-10-14 17:23:14 -0400

    Update Add  Specific User Last Login Date to UDF 18.ps1

--- Add  Specific User Last Login Date to UDF 18.ps1
+++ Add  Specific User Last Login Date to UDF 18.ps1
@@ -1,7 +1,21 @@
-$username = "<Insert User Name>"
-$udf18 = (get-localuser -Name $username ).LastLogon
+$username = "<Insert User Name>" #doesn't work for Azure AD / Entra Accounts
+$udf18 = (get-localuser -Name $username ).LastLogon 
 Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom18$env:usrUDF18" -Value $udf18
 $udf18
 <#Feature road map
 Would like to check for the most recent user of the administrators security group that has signed in not just the most recently signed in.
+
+Psudo code for future update:
+
+$admins = (Get-LocalGroupMember -Group Administrators).name
+
+foreach ($admin in $admins){
+    $logindate.add = (get-localuser -Name $admin).LastLogon} #having issues with this section for mixed account types
+
+
+$udf18 = $logindate -sort | select 1 #select the most recent date from list
+Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom18$env:usrUDF18" -Value $udf18
+$udf18
+
 #>
+
