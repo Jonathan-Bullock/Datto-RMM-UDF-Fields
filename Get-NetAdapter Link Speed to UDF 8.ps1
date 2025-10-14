47e260f1a0d03c8916ff868f27f89677802babbf
Jonathan-Bullock <88416014+Jonathan-Bullock@users.noreply.github.com>
2025-10-14 16:23:54 -0400

    Update Get-NetAdapter Link Speed to UDF 8.ps1
    Updated to include Wi-Fi Signal Strength when connected to Wi-Fi

--- Get-NetAdapter Link Speed to UDF 8.ps1
+++ Get-NetAdapter Link Speed to UDF 8.ps1
@@ -1,10 +1,12 @@
-﻿<# Gets interface link Speed and write to UDF
-
-Created by Jonathan Bullock with Hatz AI
+<#----------------------08/09/2025----------------------
+Updated to add Wi-Fi Signal Strength
+-Jonathan Bullock
 #>
 
 $Adapters = Get-NetAdapter | Where-Object {($_.LinkSpeed -ne '0 bps') -and ($_.Name -like "Ethernet*" -or $_.Name -like "Wi-Fi*") }
 $sting = ""
+
+
 foreach ($Adapter in $Adapters) 
 {
     $string += $adapter.Name
@@ -12,9 +14,18 @@ foreach ($Adapter in $Adapters)
     $string += $adapter.Status
     $string += " "
     $string += $adapter.LinkSpeed
+    if (($adapter.Name -eq "Wi-Fi") -and ($adapter.Status -eq "Up")){
+        $SignaStrength = ((netsh wlan show interfaces) -Match 'Signal').trim('    Signal                 :').ToString()
+        $string += " "
+        $string += "Strength: $($SignaStrength)"
+        netsh wlan show interfaces #show wi-fi interfaces in standard output but not UDF
+    }
+
     $string += ", "
 }
 
 $udf8 = ($String.TrimEnd(', ') | Out-String -Width 250)
+
+$udf8
 
 Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom8$env:usrUDF8" -Value $udf8
