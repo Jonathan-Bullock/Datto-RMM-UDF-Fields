961a6c1a2e0d858b1b6e2582e6b4af6afea2b979
Jonathan-Bullock <jonathanbullock94@yahoo.com>
2026-04-01 15:09:00 -0400

    Create logon Days.ps1

---
+++ logon Days.ps1
@@ -0,0 +1,7 @@
+﻿$quserResult = quser /server:$env:COMPUTERNAME 2>&1 #get user login status
+$quserRegex = $quserResult | ForEach-Object -Process { $_ -replace '\s{2,}',',' } #convert to CSV
+$quserObject = $quserRegex | ConvertFrom-Csv #convert to object type
+$quserObject #write data
+$udf = ((Get-Date) - [datetime]($quserObject.'LOGON TIME')).Days #get amound of time user has been logged in days
+$udf26 = ($udf | Out-String -Width 250)
+Set-ItemProperty "HKLM:\Software\CentraStage" -Name "Custom26$env:usrUDF26" -Value $udf26
